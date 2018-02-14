84e493ef3ee4e189c579bcfb4124d506d975e9e9
Darren Smith <nerevar@gmail.com>
2018-02-13 22:04:51 -0600

    Update build fingerprint
    Update to latest 4.4.4 version for our device which was KTU84P.
    
    Change-Id: I89bdd807fd37843f8a52c029ef0ee0150e8e2f59

--- slim.mk
+++ slim.mk
@@ -22,4 +22,4 @@ PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49H 937116 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4.4/KTU84P/1227136:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4.4 KTU84P 1227136 release-keys"
